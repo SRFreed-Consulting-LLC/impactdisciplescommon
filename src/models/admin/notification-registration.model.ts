7cc1f9b3369fd0331d36da21292be0814df7048f
Shane Freed <shane.freed@gmail.com>
2024-08-01 12:31:07 -0400

    changed a few notification attributes

--- src/models/admin/notification-registration.model.ts
+++ src/models/admin/notification-registration.model.ts
@@ -1,7 +1,9 @@
+import { Timestamp } from "@google-cloud/firestore";
+
 export class NotificationRegistrationModel {
   id: string;
   email: string;
   fcmId: string;
-  dateRegistered: Date;
-  dateRemoved: Date;
+  dateRegistered: Timestamp;
+  dateRemoved: Timestamp;
 }
