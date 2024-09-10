211113f519f3ea085e3a417620e6c795fafe7d2c
Shane Freed <shane.freed@gmail.com>
2024-09-10 09:28:29 -0400

    added consultations

---
+++ src/models/domain/consultation-survey.model.ts
@@ -0,0 +1,21 @@
+import { Timestamp } from "firebase/firestore";
+import { BaseModel } from "../base.model";
+import { Phone } from "./utils/phone.model";
+
+export class ConsultationSurveyModel extends BaseModel{
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: Phone;
+  churchName?: string;
+  country?: string;
+  commitment?: number;
+  readiness?: number;
+  strategyDescription?: string;
+  teamDescription?: string;
+  communicationDescription?: string;
+  resourceDescription?: string;
+  resultsDescription?: string;
+  supportDescription?: string;
+  date: Timestamp
+}
