bf0089fc1bffedeba14bed7d6923e91f8957b64d
Shane Freed <shane.freed@gmail.com>
2024-09-17 22:38:44 -0400

    changed names for date fields

--- src/models/domain/seminar.model.ts
+++ src/models/domain/seminar.model.ts
@@ -6,12 +6,12 @@ import { Phone } from "./utils/phone.model";
 export class SeminarModel extends BaseModel{
   date: Timestamp;
   dateString: string;
-  requestedSeminarDate: Timestamp;
-  requestedSeminarDateString: string;
-  requestedStartDateTime: Timestamp;
-  requestedStartDateTimeString: string;
-  requestedEndDateTime: Timestamp;
-  requestedEndDateTimeString: string;
+  requestedDate: Timestamp;
+  requestedDateString: string;
+  requestedStartTime: Timestamp;
+  requestedStartTimeString: string;
+  requestedEndTime: Timestamp;
+  requestedEndTimeString: string;
   preferredTrainer: string;
   preferredLocationName: string;
   preferredLocation: Address;
--- src/services/seminar.service.ts
+++ src/services/seminar.service.ts
@@ -16,9 +16,9 @@ export class SeminarService {
   getAll(): Promise<SeminarModel[]>{
     return this.dao.getAll(this.table).then(items => {
       items.forEach(item => {
-        item.requestedSeminarDate = dateFromTimestamp(item.requestedSeminarDate);
-        item.requestedEndDateTime = dateFromTimestamp(item.requestedEndDateTime);
-        item.requestedStartDateTime = dateFromTimestamp(item.requestedStartDateTime);
+        item.requestedDate = dateFromTimestamp(item.requestedDate);
+        item.requestedEndTime = dateFromTimestamp(item.requestedEndTime);
+        item.requestedStartTime = dateFromTimestamp(item.requestedStartTime);
       })
 
       return items;
@@ -28,10 +28,10 @@ export class SeminarService {
   streamAll(): Observable<SeminarModel[]>{
     return this.dao.streamAll(this.table).pipe(
       map(events => {
-        events.forEach(event => {
-          event.requestedSeminarDate = dateFromTimestamp(event.requestedSeminarDate as Timestamp);
-          event.requestedStartDateTime = dateFromTimestamp(event.requestedStartDateTime as Timestamp);
-          event.requestedEndDateTime = dateFromTimestamp(event.requestedEndDateTime as Timestamp);
+        events.forEach(item => {
+          item.requestedDate = dateFromTimestamp(item.requestedDate);
+          item.requestedEndTime = dateFromTimestamp(item.requestedEndTime);
+          item.requestedStartTime = dateFromTimestamp(item.requestedStartTime)
         });
         return events;
       })
