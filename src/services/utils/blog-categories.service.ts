3885c70c307442a966641fabab8257ca1a36264c
gabbyfreed-co <gabby.freed@srfreedconsulting.com>
2024-09-23 15:57:17 -0400

    Add blog Categories

---
+++ src/services/utils/blog-categories.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { FirebaseDAO } from 'impactdisciplescommon/src/dao/firebase.dao';
+import { TagModel } from 'impactdisciplescommon/src/models/domain/tag.model';
+import { CategoryModel } from 'impactdisciplescommon/src/models/utils/categories.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogCategoriesService {
+  table: string = 'blog_categories';
+
+  constructor(public dao: FirebaseDAO<TagModel>) {}
+
+  getAll(): Promise<TagModel[]>{
+    return this.dao.getAll(this.table)
+  }
+
+  streamAll(): Observable<TagModel[]>{
+    return this.dao.streamAll(this.table);
+  }
+
+  getAllByValue(field: string, value: any): Promise<TagModel[]>{
+    return this.dao.getAllByValue(this.table, field, value);
+  }
+
+  getById(id: String): Promise<TagModel>{
+    return this.dao.getById(id, this.table);
+  }
+
+  add(value: TagModel): Promise<TagModel>{
+    return this.dao.add(value, this.table);
+  }
+
+  update(id: string, value: TagModel): Promise<TagModel>{
+    return this.dao.update(id, value, this.table);
+  }
+
+  delete(id: string){
+    return this.dao.delete(id, this.table);
+  }
+}
