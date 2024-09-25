beb3de8047ef0e087c19080e5965bc89d8726e7b
gabbyfreed-co <gabby.freed@srfreedconsulting.com>
2024-09-24 22:13:58 -0400

    make address model more dynamic

--- src/models/domain/utils/address.model.ts
+++ src/models/domain/utils/address.model.ts
@@ -1,10 +1,10 @@
 export class Address {
-    address1: string;
-    address2: string;
-    city: string;
-    state: string;
-    zip: string;
-    country: string;
+    address1?: string;
+    address2?: string;
+    city?: string;
+    state?: string;
+    zip?: string;
+    country?: string;
 
     constructor(){}
 }
