1fc1486ddc1d227ea70786143e1a86b65e65c0a8
Shane Freed <shane.freed@gmail.com>
2025-01-23 11:29:56 -0500

    added breakoutflag

--- src/models/domain/utils/agenda-item.model.ts
+++ src/models/domain/utils/agenda-item.model.ts
@@ -8,6 +8,7 @@ export class AgendaItem extends BaseModel{
   course?: string;
   coaches?: string[];
   isCourse?: boolean;
+  isBreakout?: boolean;
   isFoodBreak?: boolean;
   maxParticipants?: number;
   signedUp?: number;
