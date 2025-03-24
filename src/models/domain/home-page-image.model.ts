fbdff6bccb1071db4669b2b70dc58654a3997d68
Shane Freed <shane.freed@gmail.com>
2025-03-24 16:41:59 -0400

    added cta and order to images

--- src/models/domain/home-page-image.model.ts
+++ src/models/domain/home-page-image.model.ts
@@ -4,9 +4,13 @@ import { ImageModel } from '../utils/image.model';
 
 export class HomePageImageModel extends BaseModel {
   isActive: boolean = false;
+  order: number;
   image?: ImageModel;
   title: string;
   text?: string;
   side: string;
+  ctaVisible: boolean;
+  ctaTitle: string;
+  ctaDestination: string;
   date: Timestamp;
 }
